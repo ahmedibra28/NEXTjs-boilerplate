d1d5ff27da176b3e72c9084fc57258045ffc1132
Ahmed <ahmaat19@gmail.com>
2021-08-15 15:54:46 +0300

    adding bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    typeof document !== undefined
+      ? require('bootstrap/dist/js/bootstrap')
+      : null
+  }, [])
   return <Component {...pageProps} />
 }
 
