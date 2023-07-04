d28f5257e244ef6d389272791100d77943584b86
Ahmed <ahmaat19@gmail.com>
2023-07-04 10:53:12 +0300

    removing webpack alias

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
     images: {
         domains: ['github.com', 'ui-avatars.com']
     },
-    webpack: (config) => {
-        config.resolve.alias['@'] = path.resolve(__dirname);
-        return config;
-    },
 }
 
 module.exports = nextConfig
