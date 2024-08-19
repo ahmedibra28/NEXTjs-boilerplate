68fe8c23cabce862b5185b35103120aa0207453e
Ahmed Ibrahim <54572773+ahmedibra28@users.noreply.github.com>
2024-08-19 08:23:36 +0300

    update: skeleton loading

--- components/skeleton.tsx
+++ components/skeleton.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Card } from '@/components/ui/card'
 
 const Skeleton = () => {
   return (
-    <div className='overflow-x-auto bg-white p-3 mt-2 mx-auto'>
-      <div className='flex items-center flex-col mb-2'>
-        <h1 className='font-light text-2xl animate-pulse dark:bg-gray-700 w-full md:w-[40%] h-5'></h1>
-        <h1 className='font-light text-2xl animate-pulse dark:bg-gray-700 w-28 h-10 mt-5'></h1>
+    <Card className='overflow-x-auto p-4 mt-2 mx-auto'>
+      <div className='flex flex-col mb-2 mt-4'>
+        <h1 className='font-light text-2xl animate-pulse bg-gray-300 dark:bg-gray-700 w-full md:w-[10%] h-5 mr-auto'></h1>
 
-        <h1 className='font-light text-2xl animate-pulse dark:bg-gray-700 w-64 h-12 mt-5'></h1>
+        <div className='flex flex-row items-center justify-between mt-4'>
+          <h1 className='font-light text-2xl animate-pulse bg-gray-300 dark:bg-gray-700 w-64 h-12 mt-5 mr-auto rounded'></h1>
+          <div className='flex flex-row items-center justify-between gap-x-5'>
+            <h1 className='font-light text-2xl animate-pulse bg-gray-300 dark:bg-gray-700 w-28 h-10 mt-5 rounded'></h1>
+            <h1 className='font-light text-2xl animate-pulse bg-gray-300 dark:bg-gray-700 w-28 h-10 mt-5 rounded'></h1>
+          </div>
+        </div>
         <div className='w-full sm:w-[80%] md:w-[50%] lg:w-[30%] mx-auto'></div>
       </div>
 
@@ -24,7 +30,7 @@ const Skeleton = () => {
           </div>
         ))}
       </div>
-    </div>
+    </Card>
   )
 }
 
